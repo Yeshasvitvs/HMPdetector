d5f6187f713eb1f1b2de73c42beeacbc746c9bcd
Yeshasvi TVS <yeshasvi.yeshi@gmail.com>
2015-04-29 13:54:59 +0200

    Update HMPdetector.hpp

--- include/HMPdetector/HMPdetector.hpp
+++ include/HMPdetector/HMPdetector.hpp
@@ -1,4 +1,10 @@
-
+//===============================================================================//
+// Name			: HMPdetector.hpp
+// Author(s)	: Yeshasvi TVS 
+// Affiliation	: University of Genova, Italy - dept. DIBRIS
+// Version		: 1.0
+// Description	: HMPdetector header file
+//===============================================================================//
 #ifndef HMPDETECTOR_HPP_
 #define HMPDETECTOR_HPP_
 
